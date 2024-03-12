fb7deee514dbf9448e2df5a8338e217638128aa1
DP <137183901+Darshit88@users.noreply.github.com>
2024-03-12 14:50:13 +0530

    LabWork-4.1

---
+++ LabWork-4.1/formula.c
@@ -0,0 +1,11 @@
+// Write a Program to find the formula's answer (x+y)^2.
+#include <stdio.h>
+int main()
+{
+    double x, y, ans;
+    printf("Enter the value x&y: ");
+    scanf("%lf %lf", &x, &y);
+    ans = (x + y) * (x + y);
+    printf("The result is: %.2f\n", ans);
+    return 0;
+}
