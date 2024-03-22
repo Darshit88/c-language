dffe3cb0d0c50ab31f9ccc57bdc03e0d1366fb36
DP <137183901+Darshit88@users.noreply.github.com>
2024-03-22 17:32:00 +0530

    LabWork-8.3

---
+++ LabWork-8.3/2dArry.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c, i, j, sum = 0;
+    float avg;
+
+    printf("Enter the row size: ");
+    scanf("%d", &r);
+    printf("Enter the column size: ");
+    scanf("%d", &c);
+
+    int a[r][c];
+    printf("Enter array elements:\n");
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            printf("a[%d][%d] = ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            sum += a[i][j];
+        }
+    }
+
+    avg = (float)sum / (r * c);
+    printf("Average is: %.2f\n", avg);
+
+    return 0;
+}
