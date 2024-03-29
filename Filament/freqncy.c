8bfc30969b2e1df8046e53fdfd3dc595f6576003
DP <137183901+Darshit88@users.noreply.github.com>
2024-03-29 17:09:24 +0530

    Filament

---
+++ Filament/freqncy.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 100
+int main()
+{
+    char str[MAX_SIZE];
+    int i, j, freq;
+    printf("Enter any string: ");
+    fgets(str, MAX_SIZE, stdin);
+
+    str[strcspn(str, "\n")] = '\0';
+
+    printf("Frequency of each letter:\n");
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        freq = 1;
+        if (str[i] == '0')
+            continue;
+        for (j = i + 1; str[j] != '\0'; j++)
+        {
+            if (str[i] == str[j])
+            {
+                freq++;
+                str[j] = '0';
+            }
+        }
+        printf("%c => %d\n", str[i], freq);
+    }
+
+    return 0;
+}
